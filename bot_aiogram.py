2f0d2f2c0d8c957ddb16e196cdcba919c006ff78
julmaty <ymatyunina@yandex.ru>
2024-07-22 20:17:26 +0300

    config_reader

--- bot_aiogram.py
+++ bot_aiogram.py
@@ -3,12 +3,12 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
-from config import TOKEN
+from config_reader import config
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token=TOKEN)
+bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher()
 
 
