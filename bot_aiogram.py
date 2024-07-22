0e0aa23383071a98ba30b191efe837d8c1d2f41c
julmaty <ymatyunina@yandex.ru>
2024-07-22 19:51:59 +0300

    first echo bot

---
+++ bot_aiogram.py
@@ -0,0 +1,37 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+from config import TOKEN
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def process_start_command(message: types.Message):
+    await message.answer("Привет!\nНапиши мне что-нибудь!")
+
+
+@dp.message(Command("help"))
+async def process_help_command(message: types.Message):
+    await message.answer("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+
+@dp.message()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
